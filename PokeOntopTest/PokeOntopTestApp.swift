e8329865fb24dce62e284c2c50acb51b09293302
Igor Maximo <igormaximo_1989@hotmail.com>
2025-01-27 21:20:50 -0300

    Initial Commit

---
+++ PokeOntopTest/PokeOntopTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeOntopTestApp.swift
+//  PokeOntopTest
+//
+//  Created by Igor Maximo on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PokeOntopTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
