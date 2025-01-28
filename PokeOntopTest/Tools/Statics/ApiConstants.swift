8d2f210d460387efdfba098745bb8ad1623f0b1d
Igor Maximo <igormaximo_1989@hotmail.com>
2025-01-27 21:25:24 -0300

    feat: defining network layer and basic structure

---
+++ PokeOntopTest/Tools/Statics/ApiConstants.swift
@@ -0,0 +1,14 @@
+//
+//  ApiConstants.swift
+//  PokeTest
+//
+//  Created by Igor Maximo on 27/01/25.
+//
+
+import Foundation
+
+internal final class ApiConstants {
+    
+    static let baseURL = "https://pokeapi.co/api/v2/"
+    
+}
