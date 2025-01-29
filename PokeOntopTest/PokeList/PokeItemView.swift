37d078509544a8a0b8824321eb5bf5c83662473f
Igor Maximo <igormaximo_1989@hotmail.com>
2025-01-28 21:51:14 -0300

    feat: improvment layout

---
+++ PokeOntopTest/PokeList/PokeItemView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct PokeItemView: View {
+    let id: Int
+    let name: String
+
+    var body: some View {
+        ZStack {
+            // Fundo cinza com bordas arredondadas
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.gray.opacity(0.2))
+                .frame(height: 140)
+
+            VStack(spacing: 0) {
+                AsyncImage(url: ApiConstants.createPokemonImageURL(for: id)) { image in
+                    image.resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .offset(y: -24)
+                } placeholder: {
+                    ProgressView()
+                }
+                
+                Text("#\(String(format: "%03d", id)) \(name.capitalized)")
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 5)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.black.opacity(0.7))
+                    )
+                    .offset(y: -15)
+            }
+        }
+//        .frame(width: 150, height: 120)
+    }
+}
+
